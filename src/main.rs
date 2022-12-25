16c4c213c37f43abdb724a2b44a46d45cc1002aa
jdxgfty <62816854+jdxgfty@users.noreply.github.com>
2022-12-26 01:17:29 +0200

    main.rs: Format file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, HttpRequest};
 use actix_files::NamedFile;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use log::error;
 
 async fn stop_horny(req: HttpRequest) -> impl Responder {
     const ALLOWED_METHODS: [&str; 2] = ["GET", "HEAD"];
     let method = req.method().as_str();
     if !ALLOWED_METHODS.contains(&method) {
-        return HttpResponse::MethodNotAllowed().insert_header(("Allow", "GET, HEAD")).finish()
+        return HttpResponse::MethodNotAllowed()
+            .insert_header(("Allow", "GET, HEAD"))
+            .finish();
     }
 
     let r = NamedFile::open("stop_horny.jpg");
@@ -16,16 +18,17 @@ async fn stop_horny(req: HttpRequest) -> impl Responder {
         Err(e) => {
             error!("{e}");
             HttpResponse::InternalServerError()
-                                .insert_header(("Content-Type", "text/plain"))
-                                .body("an unknown error occured\n")
-        },
+                .insert_header(("Content-Type", "text/plain"))
+                .body("an unknown error occured\n")
+        }
     }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(
-        env_logger::Env::default().default_filter_or("actix_server::builder=info,actix_server::server=info")
+        env_logger::Env::default()
+            .default_filter_or("actix_server::builder=info,actix_server::server=info"),
     );
 
     HttpServer::new(|| {
